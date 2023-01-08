76d7e3f26988fbff36d3c597e2bd12580a9b26cb
Jinesh Shah <jineshbshah09@gmail.com>
2023-01-08 14:01:32 +0530

    Revert "Updated nsg"
    This reverts commit cef57b4fd7438e3dd8e2cfdae53f14efc1a3e7ae.

--- ec2/variable.tf
+++ ec2/variable.tf
@@ -1,6 +1,5 @@
 variable "ami" {}
 variable "instance_type" {}
-variable "vpc_id" {}
 variable "subnet_id" {}
 variable "key_name" {}
 variable "iam_instance_profile" {}
@@ -8,6 +7,9 @@ variable "bucket_url" {}
 variable "availability_zone" {}
 variable "ebs_volume_size" {}
 variable "ebs_att_device_name" {}
+variable "security_groups" {
+  type = list(string)
+}
 variable "tags" {
   type = map(string)
 }
--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ module "ec2" {
   ami                  = var.ami
   instance_type        = var.instance_type
   key_name             = var.key_name
-  vpc_id               = var.vpc_id
   subnet_id            = var.subnet_id
   iam_instance_profile = var.iam_instance_profile
   bucket_url           = var.bucket_url
+  security_groups      = var.security_groups
   ebs_att_device_name  = var.ebs_att_device_name
   availability_zone    = var.availability_zone
   ebs_volume_size      = var.ebs_volume_size
